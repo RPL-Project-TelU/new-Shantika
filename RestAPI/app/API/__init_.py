afb6e2f2625fa515a4bf285b5a4016954cd11b88
degenerate <dawskutel@gmail.com>
2022-07-02 04:53:26 +0700

    fix rest API dan add flask.py sebisanya :)

--- RestAPI/app/API/__init_.py
+++ RestAPI/app/API/__init_.py
@@ -1,29 +1,28 @@
-from apispec import APISpec
-
-
 try:
     from flask import Flask
     from flask_restful import Resource, Api
-    from apispec import APIspec
-    from marshmallow import Schema, fiields
+    from apispec import APISpec
+    from marshmallow import Schema, fields
     from apispec.ext.marshmallow import MarshmallowPlugin
     from flask_apispec.extension import FlaskApiSpec
     from flask_apispec.views import MethodResource
     from flask_apispec import marshal_with, doc, use_kwargs
 
-    from API.ClusterHealth.view import HealthController
-except Exception as e:
-    print("__init Modules Tidak Ditemukan{}".format(e))
+    from API.ClusterHealth.view import HeathController
 
-app = Flask(__name__)
-api = API(app)
+except Exception as e:
+    print("__init Modules are Missing {}".format(e))
+
+app = Flask(__name__)  # Flask app instance initiated
+api = Api(app)  # Flask restful wraps Flask app around it.
 app.config.update({
     'APISPEC_SPEC': APISpec(
-    title = 'Awesome Project',
-    version='v1'
-    plugins=[MarshmallowPlugin()]
-    openapi_version='2.0.0'),
-    'APISPEC_SWAGGER_URL': '/swagger/',
-    'APISPEC_SWAGGER_UI_URL': '/swaggerui/'
+        title='Awesome Project',
+        version='v1',
+        plugins=[MarshmallowPlugin()],
+        openapi_version='2.0.0'
+    ),
+    'APISPEC_SWAGGER_URL': '/swagger/',  # URI to access API Doc JSON
+    'APISPEC_SWAGGER_UI_URL': '/swagger-ui/'  # URI to access UI of API Doc
 })
 docs = FlaskApiSpec(app)
--- RestAPI/app/app.py
+++ RestAPI/app/app.py
@@ -1,12 +1,14 @@
- from dotenv import load_dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
- load_dotenv()
+try:
+    from API import (app,
+                     api,
+                     HeathController,docs
 
- try:
-    from API import (app,api,HealthController,docs)
-
+                     )
 except Exception as e:
-    print("Modules are Missing:{}".format(e))
+    print("Modules are Missing : {} ".format(e))
 
-api.add_resource(HealthController, '/health_check')
-docs.register(HealthController)
+api.add_resource(HeathController, '/health_check')
+docs.register(HeathController)
