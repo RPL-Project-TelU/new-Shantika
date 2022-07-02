bb60f4b48dcd28d4bd7becca748344f07b266b69
Fiffah <itsmefiffah@gmail.com>
2022-07-02 10:20:05 +0700

    final update unit testing add bus

--- testing/testingSearch.py
+++ testing/testingSearch.py
@@ -1,13 +1,15 @@
+from pickle import TRUE
+from tkinter import Button
 import unittest
 import main_project
 
 class TestProject(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(" ", " ")
+        self.assertEqual("EXIT", TRUE)
     
     def test_button(self):
-        self.assertEqual(" ", " ")
+        self.assertEqual(Button, "SEARCH")
     
-    def test_add(self):
-        self.assertEqual("user", "123")
+    def test_button(self):
+        self.assertEqual(Button, "Book Now")
