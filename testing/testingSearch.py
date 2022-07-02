64fd339be48fd0c4410bdbc968bd4fdf5d1ea9f9
Fiffah <itsmefiffah@gmail.com>
2022-07-02 10:40:12 +0700

    finalisasi unit test search bus

--- testing/testingSearch.py
+++ testing/testingSearch.py
@@ -1,15 +1,15 @@
-from pickle import TRUE
+
 from tkinter import Button
 import unittest
 import main_project
 
 class TestProject(unittest.TestCase):
 
-    def test_add(self):
-        self.assertEqual("EXIT", TRUE)
+    def test_button(self):
+        self.assertTrue(Button, "EXIT")
+
+    def test_button(self):
+        self.assertTrue(Button, "SEARCH")
     
     def test_button(self):
-        self.assertEqual(Button, "SEARCH")
-    
-    def test_button(self):
-        self.assertEqual(Button, "Book Now")
+        self.assertTrue(Button, "BOOK NOW")
