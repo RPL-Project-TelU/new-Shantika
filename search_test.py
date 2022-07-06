0e5bdcc82d5e6a5590f58de0d27d9c4ecbfa13b9
Fiffah <itsmefiffah@gmail.com>
2022-07-06 14:43:24 +0700

    update search test

---
+++ search_test.py
@@ -0,0 +1,12 @@
+import imp
+import unittest
+import design_pattern as ds
+import os
+
+class TestStringMethod(unittest.TestCase):
+
+    def test_cari(self):
+        self.assertTrue(ds.tambah_bus.__init__(self), 'Jahfal')
+
+if __name__ == '__main__':
+    unittest.main()
